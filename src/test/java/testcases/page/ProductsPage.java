8a7625c809b2982c2dda7b5b39b449c51732a08a
Kaya_tk <thillaikayatk@gmail.com>
2024-04-24 21:24:52 +0200

    About Testcase

--- src/test/java/testcases/page/ProductsPage.java
+++ src/test/java/testcases/page/ProductsPage.java
@@ -7,11 +7,23 @@ public class ProductsPage extends TestBase {
 
     WebElement menu;
     WebElement logOut;
+    WebElement about;
 
     public  void clickMenu(){
         menu=driver.findElement(By.id("react-burger-menu-btn"));
         menu.click();
     }
+    public  void clickAbout(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        about = driver.findElement(By.id("about_sidebar_link"));
+        about.click();
+    }
+
+
 
     public  void clickLogout(){
         try {
--- src/test/java/testcases/tests/AboutTest.java
+++ src/test/java/testcases/tests/AboutTest.java
@@ -4,15 +4,13 @@ import org.testng.annotations.Test;
 import testcases.functions.loginFunction;
 import testcases.page.ProductsPage;
 
-public class LogoutTest extends ProductsPage {
+public class AboutTest extends ProductsPage {
 
     @Test(dataProvider = "jsondata")
-    public void verifyLogout(String ar){
+    public void verifyAbout(String ar){
         loginFunction loginFn = new loginFunction();
         loginFn.Login(ar);
         clickMenu();
-        clickLogout();
+        clickAbout();
     }
-
-
 }
