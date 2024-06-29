58c6dc6eec53b8dd1555119daba1a7d779af885a
Kaya_tk <thillaikayatk@gmail.com>
2024-06-29 09:49:17 +0200

    script update-About Verifications

--- src/test/java/testcases/tests/AboutTest.java
+++ src/test/java/testcases/tests/AboutTest.java
@@ -2,15 +2,19 @@ package testcases.tests;
 
 import org.testng.annotations.Test;
 import testcases.functions.loginFunction;
+import testcases.page.AboutPage;
 import testcases.page.ProductsPage;
 
 public class AboutTest extends ProductsPage {
 
+    loginFunction loginFn = new loginFunction();
+    AboutPage about =new AboutPage();
+
     @Test(dataProvider = "jsondata")
     public void verifyAbout(String ar){
-        loginFunction loginFn = new loginFunction();
         loginFn.Login(ar);
         clickMenu();
         clickAbout();
+        about.verifyAboutPage();
     }
 }
